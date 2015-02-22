cf8879130e42adc95913bfc5e54e78bffe763f8f
Damir Svrtan <damir.svrtan@gmail.com>
2015-02-22 23:58:12 +0100

    Reordered method_call spec.

--- spec/lib/fasterer/method_call_spec.rb
+++ spec/lib/fasterer/method_call_spec.rb
@@ -10,119 +10,290 @@ describe Fasterer::MethodCall do
     Fasterer::MethodCall.new(call_element)
   end
 
-  describe 'method call on a constant' do
+  describe 'with explicit receiver' do
+    describe 'without arguments, without block, called with parentheses' do
+      describe 'method call on a constant' do
 
-    # let(:code) { 'method_call_on_constant.rb' }
+        let(:code) { "User.hello()" }
 
-    let(:code) { "User.hello()" }
+        # This is where the :call token will be recognized.
+        let(:call_element) { ripper.drop(1).first.first[1] }
 
-    # This is where the :call token will be recognized.
-    let(:call_element) { ripper.drop(1).first.first[1] }
-
-    it 'should detect constant' do
-      expect(method_call.method_name).to eq('hello')
-      expect(method_call.arguments).to be_empty
-    end
-
-  end
-
-  describe 'method call on a integer' do
-
-    let(:code) { '1.hello()' }
-
-    # This is where the :call token will be recognized.
-    let(:call_element) { ripper.drop(1).first.first[1] }
-
-    it 'should detect integer' do
-      expect(method_call.method_name).to eq('hello')
-      expect(method_call.arguments).to be_empty
-    end
-
-  end
-
-  describe 'method call on a string' do
-
-    let(:code) { "'hello'.hello()" }
-
-    let(:call_element) { ripper.drop(1).first.first[1] }
-
-    it 'should detect string' do
-      expect(method_call.method_name).to eq('hello')
-      expect(method_call.arguments).to be_empty
-    end
-
-  end
-
-  describe 'method call on a variable' do
-
-    let(:code) do
-      "number_one = 1\n"\
-      "number_one.hello()"
-    end
-
-    let(:call_element) { ripper.drop(1).first.last[1] }
-
-    it 'should detect variable' do
-      expect(method_call.method_name).to eq('hello')
-      expect(method_call.arguments).to be_empty
-      expect(method_call.receiver).to be_a(Fasterer::VariableReference)
-    end
-
-  end
-
-  describe 'method call on a method' do
-
-    let(:code) { '1.hi(2).hello()' }
-
-    let(:call_element) { ripper.drop(1).first.first[1] }
-
-    it 'should detect method' do
-      expect(method_call.method_name).to eq('hello')
-      expect(method_call.receiver).to be_a(Fasterer::MethodCall)
-      expect(method_call.receiver.name).to eq('hi')
-      expect(method_call.arguments).to be_empty
-    end
-
-  end
-
-  describe 'method call with equals operator' do
-
-    let(:code) { 'method_call_with_equals.rb' }
-
-    let(:call_element) { ripper.drop(1).first.first[1] }
-
-    xit 'should recognize receiver' do
-      # expect(method_call.method_name).to eq('hello')
-      # expect(method_call.receiver).to be_a(Fasterer::MethodCall)
-      # expect(method_call.receiver.name).to eq('hi')
-    end
-
-  end
-
-
-  # Needed for Hash.fetch. Need to do more reconstruction,
-  # before recognizing method calls arguments.
-  describe 'method call with a block' do
-
-    let(:code) do
-      <<-code
-        number_one = 1
-        number_one.fetch do |el|
-          number_two = 2
-          number_three = 3
+        it 'should detect constant' do
+          expect(method_call.method_name).to eq('hello')
+          expect(method_call.arguments).to be_empty
         end
-      code
+
+      end
+
+      describe 'method call on a integer' do
+
+        let(:code) { '1.hello()' }
+
+        # This is where the :call token will be recognized.
+        let(:call_element) { ripper.drop(1).first.first[1] }
+
+        it 'should detect integer' do
+          expect(method_call.method_name).to eq('hello')
+          expect(method_call.arguments).to be_empty
+        end
+
+      end
+
+      describe 'method call on a string' do
+
+        let(:code) { "'hello'.hello()" }
+
+        let(:call_element) { ripper.drop(1).first.first[1] }
+
+        it 'should detect string' do
+          expect(method_call.method_name).to eq('hello')
+          expect(method_call.arguments).to be_empty
+        end
+
+      end
+
+      describe 'method call on a variable' do
+
+        let(:code) do
+          "number_one = 1\n"\
+          "number_one.hello()"
+        end
+
+        let(:call_element) { ripper.drop(1).first.last[1] }
+
+        it 'should detect variable' do
+          expect(method_call.method_name).to eq('hello')
+          expect(method_call.arguments).to be_empty
+          expect(method_call.receiver).to be_a(Fasterer::VariableReference)
+        end
+
+      end
+
+      describe 'method call on a method' do
+
+        let(:code) { '1.hi(2).hello()' }
+
+        let(:call_element) { ripper.drop(1).first.first[1] }
+
+        it 'should detect method' do
+          expect(method_call.method_name).to eq('hello')
+          expect(method_call.receiver).to be_a(Fasterer::MethodCall)
+          expect(method_call.receiver.name).to eq('hi')
+          expect(method_call.arguments).to be_empty
+        end
+
+      end
     end
 
-    let(:call_element) { ripper.drop(1).first.last }
+    describe 'without arguments, without block, called without parentheses' do
+      describe 'method call on a constant' do
 
-    it 'should detect block' do
-      expect(method_call.method_name).to eq('fetch')
-      expect(method_call.arguments).to be_empty
-      expect(method_call.has_block?).to be
-      expect(method_call.receiver).to be_a(Fasterer::VariableReference)
+        let(:code) { "User.hello" }
+
+        # This is where the :call token will be recognized.
+        let(:call_element) { ripper.drop(1).first.first }
+
+        it 'should detect constant' do
+          expect(method_call.method_name).to eq('hello')
+          expect(method_call.arguments).to be_empty
+        end
+
+      end
+
+      describe 'method call on a integer' do
+
+        let(:code) { '1.hello' }
+
+        # This is where the :call token will be recognized.
+        let(:call_element) { ripper.drop(1).first.first }
+
+        it 'should detect integer' do
+          expect(method_call.method_name).to eq('hello')
+          expect(method_call.arguments).to be_empty
+        end
+
+      end
+
+      describe 'method call on a string' do
+
+        let(:code) { "'hello'.hello" }
+
+        let(:call_element) { ripper.drop(1).first.first }
+
+        it 'should detect string' do
+          expect(method_call.method_name).to eq('hello')
+          expect(method_call.arguments).to be_empty
+        end
+
+      end
+
+      describe 'method call on a variable' do
+
+        let(:code) do
+          "number_one = 1\n"\
+          "number_one.hello"
+        end
+
+        let(:call_element) { ripper.drop(1).first.last }
+
+        it 'should detect variable' do
+          expect(method_call.method_name).to eq('hello')
+          expect(method_call.arguments).to be_empty
+          expect(method_call.receiver).to be_a(Fasterer::VariableReference)
+        end
+
+      end
+
+      describe 'method call on a method' do
+
+        let(:code) { '1.hi(2).hello' }
+
+        let(:call_element) { ripper.drop(1).first.first }
+
+        it 'should detect method' do
+          expect(method_call.method_name).to eq('hello')
+          expect(method_call.receiver).to be_a(Fasterer::MethodCall)
+          expect(method_call.receiver.name).to eq('hi')
+          expect(method_call.arguments).to be_empty
+        end
+
+      end
     end
 
+    describe 'with do end block' do
+      describe 'and no arguments' do
+
+        let(:code) do
+          <<-code
+            number_one = 1
+            number_one.fetch do |el|
+              number_two = 2
+              number_three = 3
+            end
+          code
+        end
+
+        let(:call_element) { ripper.drop(1).first.last }
+
+        it 'should detect block' do
+          expect(method_call.method_name).to eq('fetch')
+          expect(method_call.arguments).to be_empty
+          expect(method_call.has_block?).to be
+          expect(method_call.receiver).to be_a(Fasterer::VariableReference)
+        end
+
+      end
+
+      describe 'and one argument within parentheses' do
+
+        let(:code) do
+          <<-code
+            number_one = 1
+            number_one.fetch(100) do |el|
+              number_two = 2
+              number_three = 3
+            end
+          code
+        end
+
+        let(:call_element) { ripper.drop(1).first.last }
+
+        it 'should detect block' do
+          expect(method_call.method_name).to eq('fetch')
+          expect(method_call.arguments.count).to be(1)
+          expect(method_call.has_block?).to be
+          expect(method_call.receiver).to be_a(Fasterer::VariableReference)
+        end
+
+      end
+    end
+
+    describe 'with curly block' do
+      describe 'in one line' do
+
+        let(:code) do
+          <<-code
+            number_one = 1
+            number_one.fetch { |el| number_two = 2 }
+          code
+        end
+
+        let(:call_element) { ripper.drop(1).first.last }
+
+        it 'should detect block' do
+          expect(method_call.method_name).to eq('fetch')
+          expect(method_call.arguments).to be_empty
+          expect(method_call.has_block?).to be
+          expect(method_call.receiver).to be_a(Fasterer::VariableReference)
+        end
+
+      end
+
+      describe 'multi lined' do
+
+        let(:code) do
+          <<-code
+            number_one = 1
+            number_one.fetch { |el|
+              number_two = 2
+              number_three = 3
+            }
+          code
+        end
+
+        let(:call_element) { ripper.drop(1).first.last }
+
+        it 'should detect block' do
+          expect(method_call.method_name).to eq('fetch')
+          expect(method_call.arguments).to be_empty
+          expect(method_call.has_block?).to be
+          expect(method_call.receiver).to be_a(Fasterer::VariableReference)
+        end
+
+      end
+    end
+
+    describe 'with arguments, without block, called with parentheses' do
+
+      describe 'method call with an argument' do
+
+        let(:code) { '{}.fetch(:writing)' }
+
+        let(:call_element) { ripper.drop(1).first.first }
+
+        it 'should detect argument' do
+          expect(method_call.method_name).to eq('fetch')
+          expect(method_call.arguments.count).to eq(1)
+          expect(method_call.arguments.first.type).to eq(:symbol_literal)
+          # expect(method_call.receiver).to be_a(Fasterer::MethodCall)
+          # expect(method_call.receiver.name).to eq('hi')
+        end
+
+      end
+    end
+
+    describe 'arguments without parenthesis' do
+      describe 'method call with an argument' do
+
+        let(:code) { '{}.fetch :writing, :listening' }
+
+        let(:call_element) { ripper.drop(1).first.first }
+
+        it 'should detect argument' do
+          expect(method_call.method_name).to eq('fetch')
+          expect(method_call.arguments.count).to eq(2)
+          expect(method_call.arguments[0].type).to eq(:symbol_literal)
+          expect(method_call.arguments[1].type).to eq(:symbol_literal)
+          # expect(method_call.receiver).to be_a(Fasterer::MethodCall)
+          # expect(method_call.receiver.name).to eq('hi')
+        end
+
+      end
+    end
+  end
+
+  describe 'with implicit receiver' do
   end
 
   describe 'method call with an argument and a block' do
@@ -142,40 +313,6 @@ describe Fasterer::MethodCall do
       expect(method_call.arguments.first.type).to eq(:symbol_literal)
       expect(method_call.has_block?).to be
       expect(method_call.receiver).to be_a(Fasterer::VariableReference)
-    end
-
-  end
-
-  # method_add_arg
-  describe 'method call with an argument' do
-
-    let(:code) { '{}.fetch(:writing)' }
-
-    let(:call_element) { ripper.drop(1).first.first }
-
-    it 'should detect argument' do
-      expect(method_call.method_name).to eq('fetch')
-      expect(method_call.arguments.count).to eq(1)
-      expect(method_call.arguments.first.type).to eq(:symbol_literal)
-      # expect(method_call.receiver).to be_a(Fasterer::MethodCall)
-      # expect(method_call.receiver.name).to eq('hi')
-    end
-
-  end
-
-  describe 'method call with an argument without brackets' do
-
-    let(:code) { '{}.fetch :writing, :listening' }
-
-    let(:call_element) { ripper.drop(1).first.first }
-
-    it 'should detect argument' do
-      expect(method_call.method_name).to eq('fetch')
-      expect(method_call.arguments.count).to eq(2)
-      expect(method_call.arguments[0].type).to eq(:symbol_literal)
-      expect(method_call.arguments[1].type).to eq(:symbol_literal)
-      # expect(method_call.receiver).to be_a(Fasterer::MethodCall)
-      # expect(method_call.receiver.name).to eq('hi')
     end
 
   end
@@ -280,5 +417,20 @@ describe Fasterer::MethodCall do
     end
 
   end
+
+  describe 'method call with equals operator' do
+
+    let(:code) { 'method_call_with_equals.rb' }
+
+    let(:call_element) { ripper.drop(1).first.first[1] }
+
+    xit 'should recognize receiver' do
+      # expect(method_call.method_name).to eq('hello')
+      # expect(method_call.receiver).to be_a(Fasterer::MethodCall)
+      # expect(method_call.receiver.name).to eq('hi')
+    end
+
+  end
+
 
 end
