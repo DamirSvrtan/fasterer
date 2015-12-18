3a1094fcd5d62d431d8bc72012b427711513093c
Damir Svrtan <damir.svrtan@gmail.com>
2015-12-19 00:00:23 +0100

    More file traverser specs

--- spec/lib/fasterer/file_traverser_spec.rb
+++ spec/lib/fasterer/file_traverser_spec.rb
@@ -41,17 +41,65 @@ describe Fasterer::FileTraverser do
       end
     end
 
-    context 'missing speedups key' do
+    context 'with speedups content but no exclude_paths' do
+      let(:config_file_content) do
+        "speedups:\n"\
+        "  keys_each_vs_each_key: true"
+      end
+
       before(:each) do
         create_file(Fasterer::FileTraverser::CONFIG_FILE_NAME,
-                    ['exclude_paths:'])
+                    config_file_content)
       end
 
       let(:file_traverser) { Fasterer::FileTraverser.new('.') }
 
-      it 'returns nil_config_file' do
+      it 'returns config_file with added exclude paths key' do
         expect(file_traverser.config_file)
-          .to eq(file_traverser.send(:nil_config_file))
+          .to eq('speedups' => { 'keys_each_vs_each_key' => true },
+                 'exclude_paths' => [])
+      end
+    end
+
+    context 'with exclude_paths content but no speedups' do
+      let(:config_file_content) do
+        "exclude_paths:\n"\
+        "  - 'spec/support/analyzer/*.rb'"
+      end
+
+      before(:each) do
+        create_file(Fasterer::FileTraverser::CONFIG_FILE_NAME,
+                    config_file_content)
+      end
+
+      let(:file_traverser) { Fasterer::FileTraverser.new('.') }
+
+      it 'returns config_file with added speedups key' do
+        expect(file_traverser.config_file)
+          .to eq('speedups' => {},
+                 'exclude_paths' => ['spec/support/analyzer/*.rb'])
+      end
+    end
+
+    context 'with exclude_paths and speedups content' do
+      let(:config_file_content) do
+        "speedups:\n"\
+        "  keys_each_vs_each_key: true\n"\
+        "exclude_paths:\n"\
+        "  - 'spec/support/analyzer/*.rb'"
+      end
+
+      before(:each) do
+        create_file(Fasterer::FileTraverser::CONFIG_FILE_NAME,
+                    config_file_content)
+      end
+
+      let(:file_traverser) { Fasterer::FileTraverser.new('.') }
+
+      it 'returns config_file' do
+        expect(file_traverser.config_file)
+          .to eq('speedups' => { 'keys_each_vs_each_key' => true },
+                 'exclude_paths' => ['spec/support/analyzer/*.rb'])
       end
     end
 
