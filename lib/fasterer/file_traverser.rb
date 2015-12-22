6c3333b6bb4938f72afa4c90ad3a1e5bc4c75a6d
Damir Svrtan <damir.svrtan@gmail.com>
2015-12-22 01:28:32 +0100

    Add statistics output

--- lib/fasterer/file_traverser.rb
+++ lib/fasterer/file_traverser.rb
@@ -13,11 +13,13 @@ module Fasterer
 
     attr_reader :config
     attr_reader :parse_error_paths
+    attr_accessor :offenses_total_count
 
     def initialize(path)
       @path = Pathname(path)
       @parse_error_paths = []
       @config = Config.new
+      @offenses_total_count = 0
     end
 
     def traverse
@@ -27,6 +29,7 @@ module Fasterer
         scan_file(@path)
       end
       output_parse_errors if parse_error_paths.any?
+      output_statistics
     end
 
     def config_file
@@ -35,6 +38,10 @@ module Fasterer
 
     def offenses_found?
       !!offenses_found
+    end
+
+    def scannable_files
+      all_files - ignored_files
     end
 
     private
@@ -50,11 +57,8 @@ module Fasterer
       if offenses_grouped_by_type(analyzer).any?
         output(analyzer)
         self.offenses_found = true
+        self.offenses_total_count += analyzer.errors.count
       end
-    end
-
-    def scannable_files
-      all_files - ignored_files
     end
 
     def all_files
@@ -86,6 +90,11 @@ module Fasterer
       puts 'has timed out. Unprocessable files were:'
       puts '-----------------------------------------------------'
       puts parse_error_paths
+      puts
+    end
+
+    def output_statistics
+      puts Statistics.new(self)
     end
 
     def ignored_speedups
@@ -104,6 +113,48 @@ module Fasterer
   ErrorData = Struct.new(:file_path, :error_class, :error_message) do
     def to_s
       "#{file_path} - #{error_class} - #{error_message}"
+    end
+  end
+
+  class Statistics
+    def initialize(traverser)
+      @files_inspected_count  = traverser.scannable_files.count
+      @offenses_found_count   = traverser.offenses_total_count
+      @unparsable_files_count = traverser.parse_error_paths.count
+    end
+
+    def to_s
+      [
+        inspected_files_output,
+        offenses_found_output,
+        unparsable_files_output
+      ].compact.join(', ')
+    end
+
+    def inspected_files_output
+      "#{@files_inspected_count} #{pluralize(@files_inspected_count, 'file')} inspected"
+        .colorize(:green)
+    end
+
+    def offenses_found_output
+      "#{@offenses_found_count} #{pluralize(@offenses_found_count, 'offense')} detected"
+        .colorize(:red)
+    end
+
+    def unparsable_files_output
+      return if @unparsable_files_count.zero?
+      "#{@unparsable_files_count} unparsable #{pluralize(@unparsable_files_count, 'file')} found"
+        .colorize(:red)
+    end
+
+    def pluralize(n, singular, plural = nil)
+      if n == 1
+        "#{singular}"
+      elsif plural
+        "#{plural}"
+      else
+        "#{singular}s"
+      end
     end
   end
 end
