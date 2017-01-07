4bf6fb297095c9adced100d70fa7390fc0cc3638
Thomas Leishman <thomas.leishman@plansource.com>
2017-01-06 18:00:21 -0700

    More inclusive check_symbol_to_proc rule which fixes #40

--- spec/support/analyzer/18_block_vs_symbol_to_proc.rb
+++ spec/support/analyzer/18_block_vs_symbol_to_proc.rb
@@ -28,3 +28,9 @@ route_sets.each do |routes|
 end
 
 route_sets.each { |routes| routes.finalize! }
+
+numbers = [1, 2, 3, 4, 5]
+numbers.each { |number| number.to_s }
+numbers.map { |number| number.to_s }
+numbers.any? { |number| number.even? }
+numbers.find { |number| number.even? }
