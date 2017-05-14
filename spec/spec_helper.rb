14399ebaa9f13c7be3c8b5c7f9bb91ae501061fb
Marco Costa <mmarcottulio@gmail.com>
2017-05-14 00:35:15 -0400

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 require 'bundler/setup'
 require 'simplecov'
 SimpleCov.start do
- add_filter '/spec/'
- add_filter '/vendor/'
+  add_filter '/spec/'
+  add_filter '/vendor/'
 end
 
 require 'fasterer'
