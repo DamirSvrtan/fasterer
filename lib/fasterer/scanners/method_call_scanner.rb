7a6d4d4ba04e3f0ecbb66906d6bf27f6cb777bbf
Damir Svrtan <damir.svrtan@gmail.com>
2020-02-04 21:22:27 -0800

    For readability replace unless with if

--- lib/fasterer/scanners/method_call_scanner.rb
+++ lib/fasterer/scanners/method_call_scanner.rb
@@ -132,7 +132,7 @@ module Fasterer
       return unless body_method_call.arguments.count.zero?
       return if body_method_call.has_block?
       return if body_method_call.receiver.nil?
-      return unless body_method_call.receiver.name == method_call.block_argument_names.first
+      return if body_method_call.receiver.name != method_call.block_argument_names.first
 
       add_offense(:block_vs_symbol_to_proc)
     end
